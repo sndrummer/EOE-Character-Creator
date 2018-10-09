8e4d1e0b7d100aebb597ae56f37d2436612df3ac
Samuel Nuttall <sndrummer@gmail.com>
2018-10-09 11:27:33 -0600

    working on adapter

--- app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/activities/CharacterSummary.java
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/activities/CharacterSummary.java
@@ -2,6 +2,8 @@ package edu.cs356.edgeoftheempirecharactercreator.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +14,11 @@ public class CharacterSummary extends AppCompatActivity {
 
     //Button
     private Button mSkillsInfoBtn; //skills_info_btn
+
+    //RecyclerView
+    private RecyclerView mSkillsList;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     //TextViews
     private TextView mCharacterName;
@@ -29,5 +36,13 @@ public class CharacterSummary extends AppCompatActivity {
         mSpeciesClassDescriptor = findViewById(R.id.char_species_class);
         mCharacterImg = findViewById(R.id.char_summary_img);
         mSkillsInfoBtn = findViewById(R.id.skills_info_btn);
+
+        mSkillsList = findViewById(R.id.skills_list);
+        mSkillsList.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mSkillsList.setLayoutManager(mLayoutManager);
+
+        //mAdapter = new MyAdapter(myDataset);
     }
 }
