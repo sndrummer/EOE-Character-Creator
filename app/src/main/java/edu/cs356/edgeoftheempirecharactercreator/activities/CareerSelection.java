c39e030c0e769781daa64bfbe3f7aa0727c58205
SponJ <authoritative.jra@gmail.com>
2018-10-09 17:52:55 -0600

    Reverted the gradle to SDK version 27 to get the layour preview to work.

--- app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/activities/CareerSelection.java
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/activities/CareerSelection.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
+
+import edu.cs356.edgeoftheempirecharactercreator.R;
 
 /**
  * Created by jalton on 10/9/18.
@@ -17,11 +20,18 @@ public class CareerSelection extends AppCompatActivity {
     private Button mToMainBtn;
     private Button mToBackGroundBtn;
 
+    private TextView mCharacterName;
+
     private int curSelection = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career_selection);
+
+        mCharacterName = findViewById(R.id.char_name_summary);
+
+
 
 
     }
