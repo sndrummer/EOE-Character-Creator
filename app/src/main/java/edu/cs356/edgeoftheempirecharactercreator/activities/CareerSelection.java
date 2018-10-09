13f435051528258f37328716c9170a7b4dd76f66
SponJ <authoritative.jra@gmail.com>
2018-10-09 09:23:27 -0600

    Started on the CareerSelection Activity

---
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/activities/CareerSelection.java
@@ -0,0 +1,28 @@
+package edu.cs356.edgeoftheempirecharactercreator.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by jalton on 10/9/18.
+ */
+
+public class CareerSelection extends AppCompatActivity {
+
+    private static final String TAG = "CareerSelectionTAG";
+
+    //Buttons
+    private Button mToMainBtn;
+    private Button mToBackGroundBtn;
+
+    private int curSelection = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/ui/CareerView.java
@@ -0,0 +1,31 @@
+package edu.cs356.edgeoftheempirecharactercreator.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jalton on 10/9/18.
+ */
+
+/* This is a custom view element for displaying Career info.
+ * The layout should look something like this:
+ *  ---------------------------------
+ *  | CAREER NAME                   |
+ *  |-------------------------------|
+ *  |         | - Career info in    |
+ *  |  IMAGE  | - a bulleted        |
+ *  |         | - list              |
+ *  ---------------------------------
+ */
+
+public class CareerView extends View {
+
+    public CareerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+}
