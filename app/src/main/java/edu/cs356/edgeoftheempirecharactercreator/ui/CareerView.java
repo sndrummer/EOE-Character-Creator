9c4aca3a7982f62b9f04352515066c78a7ddf920
SponJ <authoritative.jra@gmail.com>
2018-10-09 18:31:33 -0600

    Still working on creating the custom CareerView element

--- app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/ui/CareerView.java
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/ui/CareerView.java
@@ -1,9 +1,13 @@
 package edu.cs356.edgeoftheempirecharactercreator.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import edu.cs356.edgeoftheempirecharactercreator.R;
 
 /**
  * Created by jalton on 10/9/18.
@@ -22,8 +26,39 @@ import android.view.View;
 
 public class CareerView extends View {
 
+    private String mCareerName;
+    private String mCareerDescription;
+    private Paint mTextPaint;
+    private int mTextColor;
+    private float mTextHeight;
+
     public CareerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CareerView,
+                0, 0);
+
+        try {
+            mCareerName = a.getString(R.styleable.CareerView_careerName);
+            mCareerDescription = a.getString(R.styleable.CareerView_careerDescription);
+        } finally {
+            a.recycle();
+        }
+
+    }
+
+    private void init() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(mTextColor);
+        if (mTextHeight == 0) {
+            mTextHeight = mTextPaint.getTextSize();
+        } else {
+            mTextPaint.setTextSize(mTextHeight);
+        }
+
+
+
     }
 
 
