5b29b4c5793be83db759052ce7e85e310667bd9f
Samuel Nuttall <sndrummer@gmail.com>
2018-10-30 17:04:59 -0600

    added dice dialog

---
+++ app/src/main/java/edu/cs356/edgeoftheempirecharactercreator/ui/DiceInfoDialogue.java
@@ -0,0 +1,42 @@
+package edu.cs356.edgeoftheempirecharactercreator.ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import edu.cs356.edgeoftheempirecharactercreator.R;
+
+public class DiceInfoDialogue extends Dialog {
+    private String descText;
+    private Button mExitBtn;
+
+    public DiceInfoDialogue(@NonNull Context context, String descText) {
+        super(context);
+        this.descText = descText;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dice_dialog);
+        TextView descriptionView = findViewById(R.id.desc_text);
+        descriptionView.setMovementMethod(new ScrollingMovementMethod());
+        mExitBtn = findViewById(R.id.exit_btn);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        descriptionView.setText(descText);
+        mExitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+}
